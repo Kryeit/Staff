b0b33dce5a250ca69f1d75fa0391107678e14c56
muriplz <46825658+muriplz@users.noreply.github.com>
2024-03-21 16:40:36 +0100

    init

---
+++ src/main/java/com/kryeit/staff/Staff.java
@@ -0,0 +1,12 @@
+package com.kryeit.staff;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Staff implements DedicatedServerModInitializer {
+
+
+    @Override
+    public void onInitializeServer() {
+        
+    }
+}
