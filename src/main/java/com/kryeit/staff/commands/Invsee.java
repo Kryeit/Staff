376b1e801cddc647d11d990c3b015a69381ef463
muriplz <46825658+muriplz@users.noreply.github.com>
2024-03-21 17:54:39 +0100

    Fixes

--- src/main/java/com/kryeit/staff/commands/Invsee.java
+++ src/main/java/com/kryeit/staff/commands/Invsee.java
@@ -5,7 +5,11 @@ import net.fabricmc.fabric.mixin.container.ServerPlayerEntityAccessor;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -26,12 +30,16 @@ public class Invsee {
 
     }
 
+    public static GenericContainerScreenHandler createGeneric9x4(int syncId, PlayerInventory playerInventory, Inventory inventory) {
+        return new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X4, syncId, playerInventory, inventory, 4);
+    }
+
     public static void openInventory(Entity opener, Entity target) {
         if (opener instanceof ServerPlayerEntity playerOpener && target instanceof PlayerEntity playerTarget) {
             var title = Text.literal(playerTarget.getDisplayName().getString() + "'s Inventory");
 
             playerOpener.openHandledScreen(new SimpleNamedScreenHandlerFactory(
-                    (i, inventory, playerx) -> GenericContainerScreenHandler.createGeneric9x3(i, inventory, playerTarget.getInventory()),
+                    (i, inventory, playerx) -> createGeneric9x4(i, inventory, playerTarget.getInventory()),
                     title
             ));
         }
